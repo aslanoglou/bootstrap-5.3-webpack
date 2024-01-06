ee9699bf6d850237c21919ab00864db4136c8109
Jordan <i.aslanoglou@gazzetta.gr>
2024-01-07 01:00:56 +0200

    webpack entry output fixes / scss paths fixes

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,23 @@ const autoprefixer = require('autoprefixer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const miniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
-    mode: 'development', entry: './src/js/main.js', output: {
-        filename: 'main.js', path: path.resolve(__dirname, 'dist')
-    }, devServer: {
+    mode: 'development',
+    // entry: './src/js/main.js',
+    entry: {
+        main: './src/js/main.js',
+        // critical: '/src/critical.js',
+        // bootstrap: '/src/bootstrap.js',
+    },
+    output: {
+        // filename: 'main.js',
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
         static: path.resolve(__dirname, 'dist'), port: 8080, hot: true
-    }, plugins: [new HtmlWebpackPlugin({template: './src/index.html'}), new miniCssExtractPlugin()], module: {
+    },
+    plugins: [new HtmlWebpackPlugin({template: './src/index.html'}), new miniCssExtractPlugin()],
+    module: {
         rules: [{
             mimetype: 'image/svg+xml', scheme: 'data', type: 'asset/resource', generator: {
                 filename: 'icons/[hash].svg'
