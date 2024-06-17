507f64facc65d84b0ddcc140aabb8b2066d81dd0
aslanoglou <iordanis.aslanoglou@gmail.com>
2024-06-17 22:08:38 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,98 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const autoprefixer = require('autoprefixer')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
+
+function generateEntryPoints(directory) {
+    const entryPoints = {};
+    const files = fs.readdirSync(directory);
+    files.forEach(file => {
+        const fileName = path.parse(file).name;
+        entryPoints[fileName] = path.resolve(directory, file);
+    });
+    return entryPoints;
+}
+
 module.exports = {
     mode: 'development',
-    // entry: './src/js/main.js',
-    entry: {
-        main: './src/js/main.js',
-        // critical: '/src/critical.js',
-        // bootstrap: '/src/bootstrap.js',
-    },
+    entry: generateEntryPoints('./src/js'),
     output: {
-        // filename: 'main.js',
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        // Output for JS files
+        filename: 'js/[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     devServer: {
-        static: path.resolve(__dirname, 'dist'), port: 8080, hot: true
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        hot: true,
     },
-    plugins: [new HtmlWebpackPlugin({template: './src/index.html'}), new miniCssExtractPlugin()],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html', // Output filename for index.html
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/assignment.html',
+            filename: 'assignment.html', // Output filename for assignment.html
+        }),
+        new miniCssExtractPlugin({
+            filename: 'css/[name].bundle.css', // Output filename for CSS files
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/images', to: 'images' },
+                { from: 'src/fonts', to: 'fonts' },
+            ],
+        }),
+    ],
     module: {
-        rules: [{
-            mimetype: 'image/svg+xml', scheme: 'data', type: 'asset/resource', generator: {
-                filename: 'icons/[hash].svg'
-            }
-        }, {
-            test: /\.(scss)$/, use: [{
-                // Extracts CSS for each JS file that includes CSS
-                loader: miniCssExtractPlugin.loader
-            }, {
-                // Interprets `@import` and `url()` like `import/require()` and will resolve them
-                loader: 'css-loader'
-            }, {
-                // Loader for webpack to process CSS with PostCSS
-                loader: 'postcss-loader', options: {
-                    postcssOptions: {
-                        plugins: [autoprefixer]
-                    }
-                }
-            }, {
-                // Loads a SASS/SCSS file and compiles it to CSS
-                loader: 'sass-loader'
-            }]
-        }]
-    }
-}
+        rules: [
+            {
+                mimetype: 'image/svg+xml',
+                scheme: 'data',
+                type: 'asset/resource',
+                generator: {
+                    filename: 'icons/[hash].svg',
+                },
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: './fonts/[name][ext]',
+                },
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        // Extracts CSS for each JS file that includes CSS
+                        loader: miniCssExtractPlugin.loader,
+                    },
+                    {
+                        // Interprets `@import` and `url()` like `import/require()` and will resolve them
+                        loader: 'css-loader',
+                    },
+                    {
+                        // Loader for webpack to process CSS with PostCSS
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [autoprefixer],
+                            },
+                        },
+                    },
+                    {
+                        // Loads a SASS/SCSS file and compiles it to CSS
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
+    },
+};
